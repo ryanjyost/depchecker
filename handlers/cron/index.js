a82c97a81b553162879548cce5574935fcb886b2
ryanjyost <ryost217@gmail.com>
2020-03-04 23:07:05 -0600

    better time

--- handlers/cron/index.js
+++ handlers/cron/index.js
@@ -2,7 +2,7 @@ const CronJob = require("cron").CronJob;
 
 const sendEmails = require("./sendEmails");
 
-const job = new CronJob("0 0 2 * * 0", sendEmails, null, true);
+const job = new CronJob("0 0 14 * * 0", sendEmails, null, true);
 
 const runCronJobs = () => {
   job.start();
